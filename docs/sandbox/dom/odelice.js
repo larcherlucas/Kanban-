1ed8f8a67c77df3e55f69bfe7770f6a62bc746c3
Clement Enjolras <enj.clement@gmail.com>
2024-03-06 12:23:16 +0100

    added odelice

---
+++ docs/sandbox/dom/odelice.js
@@ -0,0 +1,16 @@
+// ===== REVISION DU DOM =====
+
+/*
+-  querySelector
+- querySelectorAll
+- addEventListener
+- createElement
+- classList
+- style
+*/
+
+/*
+-  <template>
+  - cloner une template
+  - selecteur par attribut slot
+*/
