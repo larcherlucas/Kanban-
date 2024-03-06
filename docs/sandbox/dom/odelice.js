a5dab2d134967badf03bc23a062a909a10b4d1de
Clement Enjolras <enj.clement@gmail.com>
2024-03-06 13:12:17 +0100

    Cours DOM

--- docs/sandbox/dom/odelice.js
+++ docs/sandbox/dom/odelice.js
@@ -1,7 +1,7 @@
 // ===== REVISION DU DOM =====
 
 /*
--  querySelector
+- querySelector
 - querySelectorAll
 - addEventListener
 - createElement
@@ -10,7 +10,30 @@
 */
 
 /*
--  <template>
+- <template>
   - cloner une template
   - selecteur par attribut slot
 */
+
+// ===== Récupérer un élément du DOM =====
+
+// ====== Modifier le style d'un élément =====
+
+// ====== Ajouter une classe sur un élément
+
+// ====== Réagir à un évènement ======
+
+// ===== Create Element ====
+
+const fruits = [
+  { name: "kiwi", unitPrice: 1, quantity: 6 },
+  { name: "banane", unitPrice: 0.25, quantity: 4 },
+  { name: "durian", unitPrice: 20, quantity: 1 },
+  { name: "pommes", unitPrice: 0.5, quantity: 3 },
+];
+
+// ====== Selection de plusieurs éléments d'un seul coup ======
+
+// ====== TEMPLATE ======
+
+// ===== TEMPLATE pour chaque fruit !
