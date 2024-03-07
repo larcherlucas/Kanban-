3d2d4a3c6a94735f4be1adfbdb800cd9a4a0c727
Clement Enjolras <enj.clement@gmail.com>
2024-03-07 11:58:55 +0100

    Fetch post

--- docs/sandbox/fetch-post/post.js
+++ docs/sandbox/fetch-post/post.js
@@ -1,1 +1,34 @@
-console.log("ok");
+// On récupere la liste des POSTS
+
+//getAllPosts();
+
+async function getAllPosts() {
+  const httpResponse = await fetch(
+    "https://jsonplaceholder.typicode.com/posts"
+  );
+  const jsonData = await httpResponse.json();
+
+  console.log(jsonData);
+}
+
+// Comment je fait une requete POST avec de la data
+
+// On veux transmettre cette donné à l'API
+const data = {
+  userId: 1,
+  title: "test",
+  body: "hello world",
+};
+
+// Pour ça, j'utilise encore fetch
+const httpResponse = await fetch("https://jsonplaceholder.typicode.com/posts", {
+  method: "POST", // On fait une requette post
+  headers: { "Content-Type": "application/json" }, // On va envoyer du JSON
+  body: JSON.stringify(data), // Avec la data qu'on veux
+});
+
+console.log(httpResponse);
+
+const jsonData = await httpResponse.json();
+
+console.log(jsonData);
